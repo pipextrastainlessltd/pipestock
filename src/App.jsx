07e9c3d2d8437f9e503cfad464f7c86c2a63e0fd
pipextrastainlessltd <sales@stainlesscutpipe.co.uk>
2025-06-24 15:56:12 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import Login from './components/Login';
-import Home from './components/Home'; // Assuming Home.jsx is your main app
+import StockView from './components/StockView';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <>
-      {loggedIn ? <Home /> : <Login onSuccess={() => setLoggedIn(true)} />}
+      {loggedIn ? <StockView /> : <Login onSuccess={() => setLoggedIn(true)} />}
     </>
   );
 }
